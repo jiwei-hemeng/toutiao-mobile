6b5f4e3550e00059f1c9e5c61c735b089158470c
阆苑 <3174541442@qq.com>
2020-05-09 11:48:38 +0800

    关于autoprefixer的配置

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: {
-    autoprefixer: {
-      browsers: ['Android >= 4.0', 'iOS >= 8']
-    },
+    // autoprefixer: {
+    //   browsers: ['Android >= 4.0', 'iOS >= 8']
+    // },
     'postcss-pxtorem': {
       rootValue: 37.5,
       propList: ['*']
