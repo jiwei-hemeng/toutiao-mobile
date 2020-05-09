23a4542f96b837136357d5c954de6727816ca3ab
阆苑 <3174541442@qq.com>
2020-05-09 10:12:43 +0800

    项目初始化2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,8 @@
 <template>
   <div id="app">
+    <h2>黑马头条</h2>
+    <router-view />
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
+<style lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
-]
+const routes = []
 
 const router = new VueRouter({
   routes
