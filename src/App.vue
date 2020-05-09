1fdca728dc6f871e5698e05dfddca95a3f56ae95
阆苑 <3174541442@qq.com>
2020-05-09 11:06:25 +0800

    vant 组件的引入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <h2>黑马头条</h2>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
     <router-view />
   </div>
 </template>
