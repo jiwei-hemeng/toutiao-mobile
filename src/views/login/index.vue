fa0ecec2066765ab857f1c93c75512f934bf26c2
阆苑 <3174541442@qq.com>
2020-05-10 16:51:27 +0800

    对正则表达式的错误修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -67,7 +67,7 @@ export default {
       userFormRules: {
         mobile: [
           { required: true, message: '手机号不能为空' },
-          { pattern: /^1[3\5\7\8]\d{9}$/, message: '手机号格式错误' }
+          { pattern: /^1[3|5|7|8]\d{9}$/, message: '手机号格式错误' }
         ],
         code: [
           { required: true, message: '验证码不能为空' },
