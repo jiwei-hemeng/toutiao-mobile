00a02b2ba6b98a0c815975fdeeaa3a50ff0569fa
阆苑 <3174541442@qq.com>
2020-05-10 09:05:22 +0800

    登录页面功能与结构基本完成

---
+++ src/api/user.js
@@ -0,0 +1,9 @@
+import request from '@/utils/request'
+// 登录请求
+export const login = data => {
+  return request({
+    method: 'POST',
+    url: '/app/v1_0/authorizations',
+    data
+  })
+}
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,78 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <van-nav-bar
+      title="登录"
+      left-arrow
+      @click-left="$router.back()"
+    />
+    <van-cell-group>
+      <van-field
+        v-model="user.mobile"
+        left-icon="smile-o"
+        placeholder="请输入手机号"
+      />
+      <van-field
+        v-model="user.code"
+        clearable
+        left-icon="music-o"
+        placeholder="请输入验证码"
+      >
+        <template #button>
+          <van-button
+            size="small"
+            round
+          >发送验证码</van-button>
+        </template>
+      </van-field>
+    </van-cell-group>
+    <van-button type="info" block @click="onLogin">登录</van-button>
+  </div>
 </template>
 
 <script>
+import { login } from '@/api/user'
 export default {
-  name: 'LoginIndex'
+  name: 'LoginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '', // 手机号
+        code: '' // 验证码
+      }
+    }
+  },
+  methods: {
+    async onLogin () {
+      try {
+        const res = await login(this.user)
+        console.log(res)
+      } catch (err) {
+        console.log('登录失败！！', err)
+      }
+    }
+  }
 }
 </script>
-
-<style>
-
+<style scoped lang="less">
+.login-container {
+  .send-btn {
+    width: 76px;
+    height: 23px;
+    background-color: #ededed;
+    .van-button__text {
+      font-size: 11px;
+      color: #666666;
+    }
+  }
+  .login-btn-wrap {
+    padding: 26px 16px;
+    .login-btn {
+      background-color: #6db4fb;
+      border: none;
+      .van-button__text {
+        font-size: 15px;
+      }
+    }
+  }
+}
 </style>
