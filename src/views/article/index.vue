dc611ad6e72cac062510fd6a427b1c1943cb82d0
阆苑 <3174541442@qq.com>
2020-05-18 18:08:21 +0800

    文章标题及作者信息布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -6,6 +6,31 @@
       left-arrow
       @click-left="$router.back()"
     />
+    <div class="article-wrap">
+      <!-- 文章标题 -->
+      <h1 class="title">{{ article.title }}</h1>
+      <!-- 作者信息 -->
+      <van-cell center class="user-info">
+        <div slot="title" class="name">{{ article.aut_name }}</div>
+        <van-image
+          slot="icon"
+          class="avatar"
+          round
+          fit="cover"
+          :src="article.aut_photo"
+        />
+        <div slot="label" class="pubdate">{{ article.pubdate | relativeTime }}</div>
+        <van-button
+          class="follow-btn"
+          :type="article.is_followed ? 'default' : 'info'"
+          :icon="article.is_followed ? '' : 'plus'"
+          round
+          size="small"
+          :loading="isFollowLoading"
+          @click="onFollow"
+        >{{ article.is_followed ? '已关注' : '关注' }}</van-button>
+      </van-cell>
+    </div>
   </div>
 </template>
 
@@ -25,12 +50,56 @@ export default {
   methods: {
     async loadArticle () {
       const { data } = await getArticle(this.articleId)
-      console.log(data)
+      this.article = data.data
+    },
+    onFollow () {}
+  },
+  data () {
+    return {
+      article: {}, // 文章数据对象
+      isFollowLoading: false, // 关注用户按钮的 loading 状态
+      isCollectLoading: false // 收藏的 loading 状态
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
+.article-wrap {
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 46px;
+  bottom: 44px;
+  overflow-y: auto;
+}
+.title {
+  font-size: 20px;
+  color: #3a3a3a;
+  padding: 24px 20px 18px;
+  background-color: #fff;
+  margin: 0;
+}
+.user-info {
+  .avatar {
+    width: 35px;
+    height: 35px;
+    margin-right: 8px;
+  }
+  .name {
+    font-size: 12px;
+    color: #333333;
+  }
+  .pubdate {
+    font-size: 11px;
+    color: #b4b4b4;
+  }
+  .follow-btn {
+    width: 85px;
+    height: 29px;
+  }
+}
+ul {
+  list-style: unset;
+}
 </style>
