3f3dcc1dbfa578dd627dc9aa785f3c565de698d3
阆苑 <3174541442@qq.com>
2020-05-23 09:10:18 +0800

    处理缓存查错

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,19 @@
       因为 router-view 是一级路由出口
       keep-alive 仅对该路由出口渲染的组件有效
     -->
-    <keep-alive :include="['LayoutIndex']">
+    <keep-alive :include="cachePages">
       <router-view/>
     </keep-alive>
   </div>
 </template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'App',
+  computed: {
+    ...mapState(['cachePages'])
+  }
+}
+</script>
 
 <style lang="less"></style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,27 @@ Vue.use(Vuex)
 const USER_KEY = 'token'
 export default new Vuex.Store({
   state: {
-    user: getItem(USER_KEY)
+    user: getItem(USER_KEY),
+    cachePages: ['LayoutIndex']
   },
   mutations: {
     setItem (state, data) {
       state.user = data
       setItem(USER_KEY, state.user)
+    },
+    // 添加缓存页面
+    addCachePage (state, pageName) {
+      if (!state.cachePages.includes(pageName)) {
+        state.cachePages.push(pageName)
+      }
+    },
+
+    // 移除缓存页面
+    removeCachePage (state, pageName) {
+      const index = state.cachePages.indexOf(pageName)
+      if (index !== -1) {
+        state.cachePages.splice(index, 1)
+      }
     }
   },
   actions: {
